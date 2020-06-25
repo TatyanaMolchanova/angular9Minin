d8717666e74145b1e38d2a0f8910337862ceab56
Molchanova Tatyana <mtuspeh@gmail.com>
2020-06-25 08:41:09 +0300

    n commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent} from './post/post.component';
 import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    Post2Component
+    Post2Component,
+    Post3Component
   ],
   imports: [
     BrowserModule
