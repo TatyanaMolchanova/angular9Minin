d17a755ca487cda9a8bbe8157ea927ec41348530
Molchanova Tatyana <mtuspeh@gmail.com>
2020-06-26 09:02:57 +0300

    n commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+  obj = { a: 1, b: {c: 2} };
+  // example 1
+  // img = 'https://ensocore.com/media/61/reactjs-logo-sticker%20%281%29.jpg';
+  inputValue = '';
+  constructor() {
+    // example 1
+    // setTimeout(() => {
+    //   console.log('Timeout is over');
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png';
+    // }, 5000);
+  }
+  // onInput(event?: any) {
+  onInput(event: KeyboardEvent) {
+    console.log('Event', event)
+    // this.inputValue = event.target.value;
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+  onBlur(str: string){
+    this.inputValue = str;
+  }
+  onClick() {
+    console.log('Click!');
+  }
 }
