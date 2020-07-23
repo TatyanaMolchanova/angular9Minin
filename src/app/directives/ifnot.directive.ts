7551e0e8c7a7aa1462a62a1d94e7da7bdf1bcb5c
Molchanova Tatyana <mtuspeh@gmail.com>
2020-07-23 08:24:20 +0300

    directive

---
+++ src/app/directives/ifnot.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appIfnot]'
+})
+export class IfnotDirective {
+
+  constructor() { }
+
+}
--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
@@ -6,6 +6,7 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/co
 export class StyleDirective {
     @Input('appStyle') color: string = 'blue';
     @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string};
+    @HostBinding('style.color') elColor = null
     // @Input() fontWeight = 'normal';
     constructor(private el: ElementRef, private r: Renderer2) {
         // this.r.setStyle(this.el.nativeElement, 'color', 'blue'); // good practice more abstractional
@@ -16,10 +17,11 @@ export class StyleDirective {
         console.log(event);
     }
     @HostListener('mouseenter') onEnter() {
-        this.r.setStyle(this.el.nativeElement, 'color', this.color);
-        this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
-        this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
-        this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+        this.elColor = this.color;
+        // this.r.setStyle(this.el.nativeElement, 'color', this.color);
+        // this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
+        // this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+        // this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
         // this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
         // console.log('this.r', this.r);
         // console.log('this.fontWeight', this.fontWeight);
@@ -28,10 +30,11 @@ export class StyleDirective {
     //     this.r.setStyle(this.el.nativeElement, 'color', 'blue');
     // }
     @HostListener('mouseleave') onLeave() {
-        this.r.setStyle(this.el.nativeElement, 'color', null);
-        this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
-        this.r.setStyle(this.el.nativeElement, 'border', null);
-        this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+        this.elColor = null;
+        // this.r.setStyle(this.el.nativeElement, 'color', null);
+        // this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+        // this.r.setStyle(this.el.nativeElement, 'border', null);
+        // this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
         // this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
     }
 
