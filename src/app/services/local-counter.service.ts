e87a25ada05f9c54024ae1a8a414b84b4d7cde7c
Molchanova Tatyana <mtuspeh@gmail.com>
2020-08-04 18:40:37 +0300

    update

--- src/app/services/local-counter.service.ts
+++ src/app/services/local-counter.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class LocalCounterService {
 
-  constructor() { }
+  counter = 0;
+  increase() {
+    this.counter++;
+  }
+  decrease() {
+    this.counter--;
+  }
 }
