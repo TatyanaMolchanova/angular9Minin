4ba204c2ea968f6955dcfbf483e16bb1e2fa247d
Molchanova Tatyana <mtuspeh@gmail.com>
2020-08-20 19:19:32 +0300

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,11 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
+  appState = 'off';
 
+  handleChange() {
+    console.log(this.appState);
+  }
 }
 
 
--- src/app/switch/switch.component.ts
+++ src/app/switch/switch.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, OnInit, Provider} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SwitchComponent),
+  multi: true
+}
 
 @Component({
   selector: 'app-switch',
   templateUrl: './switch.component.html',
-  styleUrls: ['./switch.component.scss']
+  styleUrls: ['./switch.component.scss'],
+  providers: [VALUE_ACCESSOR]
 })
-export class SwitchComponent implements OnInit {
+export class SwitchComponent implements ControlValueAccessor {
+  state = 'off';
 
-  constructor() { }
+  private onChange = (value: any) => {}
 
-  ngOnInit(): void {
+  setState(state: string) {
+    this.state = state;
+    this.onChange(this.state);
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+  }
+  setDisabledState(isDisabled: boolean): void {
+  }
+  writeValue(state: string): void {
+    this.state = state;
   }
 
 }
